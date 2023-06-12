14221203249482df98b39a4e84a68747a649a543
sarahm <minutemin@gmail.com>
2023-06-11 22:46:48 -0700

    edit employees table in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -23,11 +23,12 @@ CREATE TABLE employees(
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL, 
     role_id INT,
-    department_id INT,
-    salary DECIMAL,
-    managers TEXT,
-    FOREIGN KEY
-    REFERENCES
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE CASCADE,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
     ON DELETE CASCADE
 );
 
